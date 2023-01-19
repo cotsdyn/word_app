26399dc6bed748917ed496854d247b3c50edb678
Alex White <alex@cotswolddynamics.com>
2023-01-19 14:38:00 +0000

    added RDS definition in tf

--- main.tf
+++ main.tf
@@ -9,3 +9,15 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+resource "aws_db_instance" "db" {
+  identifier           = "db-words"
+  allocated_storage    = 10
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t3.micro"
+  username             = "admin"
+  password             = "easelivedemo123"
+  parameter_group_name = "default.mysql8.0"
+  skip_final_snapshot  = true
+}
