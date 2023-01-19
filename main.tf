4d29308788b95a53694f8223a6876b1ca62d512b
Alex White <alex@cotswolddynamics.com>
2023-01-19 16:11:33 +0000

    manage the "mysql_admin_from_offices" VPC security group with tf

--- main.tf
+++ main.tf
@@ -10,12 +10,34 @@ provider "aws" {
   region = "us-east-1"
 }
 
-
+# get the "default" AWS security group in the default VPC
 data "aws_security_group" "default" {
   name = "default"
 }
-data "aws_security_group" "mysqlremote" {
-  name = "mysqlremote"
+
+# security group to allow MySQL administration from the offices
+resource "aws_security_group" "mysql_admin_from_offices" {
+  name        = "mysql_admin_from_offices"
+  description = "Allow MySQL admin from our office IPs"
+  #vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "MySQL inbound"
+    from_port        = 3306
+    to_port          = 3306
+    protocol         = "tcp"
+    cidr_blocks      = [
+      "87.115.86.44/32"  # alex's oxford office
+    ]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 }
 
 resource "aws_db_instance" "db" {
@@ -29,9 +51,10 @@ resource "aws_db_instance" "db" {
   parameter_group_name = "default.mysql8.0"
   skip_final_snapshot  = true
 
+  # allow access from our offices
   publicly_accessible    = true
   vpc_security_group_ids = [
     data.aws_security_group.default.id,
-    data.aws_security_group.mysqlremote.id
+    aws_security_group.mysql_admin_from_offices.id
   ]
 }
