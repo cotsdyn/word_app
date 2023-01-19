7be6f0a2a664c9a6d32cffa4480b0fcc27807ded
Alex White <alex@cotswolddynamics.com>
2023-01-19 23:48:16 +0000

    get the Terraformed mysql database username and password from env vars

--- main.tf
+++ main.tf
@@ -51,15 +51,25 @@ resource "aws_security_group" "mysql_admin_from_offices" {
   }
 }
 
-# MySQL database
+# the app's MySQL database
+variable "db_username" {
+  type      = string
+  nullable  = false
+}
+
+variable "db_password" {
+  type      = string
+  nullable  = false
+}
+
 resource "aws_db_instance" "db" {
   identifier           = "db-words"
   allocated_storage    = 10
   engine               = "mysql"
   engine_version       = "8.0"
   instance_class       = "db.t3.micro"
-  username             = "admin"
-  password             = "easelivedemo123"
+  username             = var.db_username
+  password             = var.db_password
   parameter_group_name = "default.mysql8.0"
   skip_final_snapshot  = true
 
