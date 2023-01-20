fbb0d1bf7c940f8bbcf544db4db119ae230e7f7d
Alex White <alex@cotswolddynamics.com>
2023-01-20 04:07:59 +0000

    re-introduce the 'db_name' option to create a database inside MySQL - it appears there is a bug with the env_variable provider where you should not use underscores...

--- main.tf
+++ main.tf
@@ -67,6 +67,7 @@ data "env_variable" "DB_PASSWORD" {
 
 resource "aws_db_instance" "db" {
   identifier           = "words"
+  db_name              = data.env_variable.DB_NAME.value
   allocated_storage    = 10
   engine               = "mysql"
   engine_version       = "8.0"
