2a0f26a95822c44f97d07d3e75a8d92b9530fd15
Alex White <alex@cotswolddynamics.com>
2023-01-20 05:21:22 +0000

    build & push both the db_init and the word_app container images

--- docker-build-and-push.sh
+++ docker-build-and-push.sh
@@ -4,9 +4,18 @@ set -e
 AWS_REGION=$(aws configure get default.region)
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
 
-# build image
-docker build -t word_app .
 
-# tag and push image to our ECR repo
-docker tag easelive-demo ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/word_app
+# build db_init image
+docker build -t db_init db_init/.
+
+# tag and push db_init image to its ECR repo
+docker tag db_init ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/db_init
+docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/db_init
+
+
+# build word_app image
+docker build -t word_app word_app/.
+
+# tag and push word_app image to its ECR repo
+docker tag word_app ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/word_app
 docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/word_app
